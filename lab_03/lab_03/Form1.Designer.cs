a29a5dad27230135bd764796a7e0d4e6829b7ec9
Luca <lucabulea18@gmail.com>
2026-01-13 11:44:58 +0200

    added lab 3

---
+++ lab_03/lab_03/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace lab_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Gray;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(656, 453);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Goldenrod;
+            button1.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(711, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(124, 34);
+            button1.TabIndex = 1;
+            button1.Text = "Translatie";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Goldenrod;
+            button2.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ControlText;
+            button2.Location = new Point(711, 72);
+            button2.Name = "button2";
+            button2.Size = new Size(124, 34);
+            button2.TabIndex = 2;
+            button2.Text = "Scalare";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Goldenrod;
+            button3.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ControlText;
+            button3.Location = new Point(711, 134);
+            button3.Name = "button3";
+            button3.Size = new Size(124, 34);
+            button3.TabIndex = 3;
+            button3.Text = "Rotatie 45";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gray;
+            ClientSize = new Size(865, 477);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ lab_03/lab_03/Form1.cs
@@ -0,0 +1,128 @@
+﻿namespace lab_03
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap;
+
+        Point[] triangle =
+        {
+            new Point(100, 100),
+            new Point(130, 30),
+            new Point(250, 100)
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            DrawPolygon(triangle, Color.Cyan);
+
+            pictureBox1.Image = bitmap;
+        }
+
+        void ClearBitmap(Color bg)
+        {
+            for (int x = 0; x < bitmap.Width; x++)
+                for (int y = 0; y < bitmap.Height; y++)
+                    bitmap.SetPixel(x, y, bg);
+        }
+
+        void SetSafePixel(int x, int y, Color c)
+        {
+            if (x < 0 || y < 0 || x >= bitmap.Width || y >= bitmap.Height) return;
+            bitmap.SetPixel(x, y, c);
+        }
+
+        void DrawLineBresenham(int x1, int y1, int x2, int y2, Color c)
+        {
+            int dx = Math.Abs(x2 - x1);
+            int dy = Math.Abs(y2 - y1);
+
+            int sx = (x1 < x2) ? 1 : -1;
+            int sy = (y1 < y2) ? 1 : -1;
+
+            int err = dx - dy;
+
+            while (true)
+            {
+                SetSafePixel(x1, y1, c);
+                if (x1 == x2 && y1 == y2) break;
+
+                int e2 = 2 * err;
+                if (e2 > -dy) { err -= dy; x1 += sx; }
+                if (e2 < dx) { err += dx; y1 += sy; }
+            }
+        }
+
+        void DrawPolygon(Point[] p, Color c)
+        {
+            for (int i = 0; i < p.Length; i++)
+            {
+                int j = (i + 1) % p.Length;
+                DrawLineBresenham(p[i].X, p[i].Y, p[j].X, p[j].Y, c);
+            }
+        }
+
+        Point[] Translate(Point[] p, int dx, int dy)
+        {
+            Point[] r = new Point[p.Length];
+            for (int i = 0; i < p.Length; i++)
+                r[i] = new Point(p[i].X + dx, p[i].Y + dy);
+            return r;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClearBitmap(Color.Gray);
+            DrawPolygon(triangle, Color.Cyan);
+            DrawPolygon(Translate(triangle, 20, 30), Color.DeepPink);
+            pictureBox1.Image = bitmap;
+        }
+
+        Point[] Scale(Point[] tri, float s)
+        {
+            Point[] result = new Point[tri.Length];
+            for (int i = 0; i < tri.Length; i++)
+                result[i] = new Point(
+                    (int)(tri[i].X * s),
+                    (int)(tri[i].Y * s)
+                );
+            return result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClearBitmap(Color.Gray);
+            DrawPolygon(triangle, Color.Cyan);
+            DrawPolygon(Scale(triangle, 2), Color.DeepPink);
+            pictureBox1.Refresh();
+        }
+
+        Point[] Rotate(Point[] p, double angle)
+        {
+            Point[] r = new Point[p.Length];
+            double c = Math.Cos(angle);
+            double s = Math.Sin(angle);
+
+            for (int i = 0; i < p.Length; i++)
+                r[i] = new Point(
+                    (int)(p[i].X * c - p[i].Y * s),
+                    (int)(p[i].X * s + p[i].Y * c)
+                );
+            return r;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ClearBitmap(Color.Gray);
+            DrawPolygon(triangle, Color.Cyan);
+            DrawPolygon(Rotate(triangle, Math.PI / 4), Color.DeepPink);
+            pictureBox1.Image = bitmap;
+        }
+    }
+}
